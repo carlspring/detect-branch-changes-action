36a9c2bd9915de92eb09b75337e4e0de7d3c2042
carlspring <carlspring@gmail.com>
2022-01-21 22:20:04 +0000

    Fixes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,22 @@ set -e
 
 BASE_BRANCH="remotes/origin/$1"
 PATHSPEC=${@:2}
-FORK_POINT_SHA=$(git merge-base --fork-point $BASE_BRANCH || git merge-base $BASE_BRANCH HEAD)
+#FORK_POINT_SHA=$(git merge-base --fork-point $BASE_BRANCH || git merge-base $BASE_BRANCH HEAD)
+#BASE_BRANCH=origin/master
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 echo ::set-output name=fork_point_sha::$FORK_POINT_SHA
 
 function check() {
 
-  if [[ -z "$(git diff --name-only $FORK_POINT_SHA $PATHSPEC)" ]];
+  if [[ -z "$(git diff --name-only $BASE_BRANCH..$CURRENT_BRANCH -- $PATH_SPEC)" ]];
   then
-    echo "Detected no changes on $PATHSPEC since $FORK_POINT_SHA"
+#    echo "Detected no changes on $PATHSPEC since $FORK_POINT_SHA"
+    echo "$BASE_BRANCH has no incoming changes for '$PATH_SPEC'"
     echo ::set-output name=changed::false
   else
-    echo "Detected changes on $PATHSPEC since $FORK_POINT_SHA"
+#    echo "Detected changes on $PATHSPEC since $FORK_POINT_SHA"
+    echo "$BASE_BRANCH has incoming changes for '$PATH_SPEC'"
     echo ::set-output name=changed::true
   fi
 }
